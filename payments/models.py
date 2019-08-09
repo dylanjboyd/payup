c0a18654750958e26087a0677e05fc5933b8ad62
Dylan Boyd <dylanjboyd@me.com>
2019-08-09 12:48:37 +1200

    Add string representations of payments models

--- payments/models.py
+++ payments/models.py
@@ -11,10 +11,16 @@ class BankRecord(models.Model):
     description = models.CharField(null=False, blank=False, max_length=200)
     amount = models.DecimalField(null=False, blank=False, max_digits=10, decimal_places=2)
 
+    def __str__(self):
+        return self.description
+
 
 class RecordMeta(models.Model):
     bank_record = models.OneToOneField(BankRecord, on_delete=models.CASCADE)
     acknowledged = models.BooleanField()
+
+    def __str__(self):
+        return str(self.bank_record)
 
 
 class AccountHolder(models.Model):
@@ -22,9 +28,15 @@ class AccountHolder(models.Model):
     reference = models.CharField(max_length=4, null=False, blank=False)
     starting_balance = models.DecimalField(null=False, blank=False, max_digits=10, decimal_places=2, default=0)
 
+    def __str__(self):
+        return self.name
+
 
 class RecordShare(models.Model):
     record_meta = models.ForeignKey(RecordMeta, on_delete=models.CASCADE)
     account_holder = models.ForeignKey(AccountHolder, on_delete=models.CASCADE)
     share = models.PositiveSmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)],
                                              null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.share}% {self.account_holder}'
